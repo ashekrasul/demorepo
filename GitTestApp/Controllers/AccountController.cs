11db15d39647ea2374ace540089af0bd14b60712
Ashek Rasul <ashekrasul@gmail.com>
2018-07-25 09:59:21 -0500

    Change 1 after main commit

--- GitTestApp/Controllers/AccountController.cs
+++ GitTestApp/Controllers/AccountController.cs
@@ -58,6 +58,7 @@ namespace GitTestApp.Controllers
         public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
+
             return View();
         }
 
