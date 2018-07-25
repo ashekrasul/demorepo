8574bc81dd373e3126ec435a4bfaec6214fa8295
dtc9376 <dtc9376@PCS7JAVA04>
2018-07-25 07:34:29 -0500

    Add project files.

---
+++ GitTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTestApp.Startup))]
+namespace GitTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
