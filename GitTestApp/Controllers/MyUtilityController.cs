d2c6e66ee6a7d6a640d6afad7354f17b6b2fdd23
Ashek Rasul <ashekrasul@gmail.com>
2018-08-02 04:19:58 -0500

    Changes done on 02/08/2018 for CR0002

---
+++ GitTestApp/Controllers/MyUtilityController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GitTestApp.Controllers
+{
+    public class MyUtilityController : ApiController
+    {
+        // GET: api/MyUtility
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MyUtility/5
+        public string Get(int id)
+        {
+            if (id == 1)
+                return "one";
+            else
+                return "no one";
+        }
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        // POST: api/MyUtility
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/MyUtility/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/MyUtility/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GitTestApp.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MyUtilityController obj = new MyUtilityController();
+            int i = obj.Add(5, 15);
+            Assert.AreEqual(20, i);
+        }
+        [TestMethod]
+        public void TestGet()
+        {
+            MyUtilityController obj = new MyUtilityController();
+            string s = obj.Get(1);
+            Assert.AreEqual("one", s);
+        }
+
+        [TestMethod]
+        public void TestGet2()
+        {
+            MyUtilityController obj = new MyUtilityController();
+            string s = obj.Get(1);
+            Assert.AreEqual("two", s);
+        }
+
+    }
+}
